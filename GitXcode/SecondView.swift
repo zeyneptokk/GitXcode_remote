2d56dd94c3c663440b6919dbf26e210630776ff2
Zeynep Tok <zeyneptok1997@gmail.com>
2022-08-16 20:54:37 +0300

    Create SecondView

---
+++ GitXcode/SecondView.swift
@@ -0,0 +1,20 @@
+//
+//  SecondView.swift
+//  GitXcode
+//
+//  Created by zeynep on 16.08.2022.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SecondView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondView()
+    }
+}
