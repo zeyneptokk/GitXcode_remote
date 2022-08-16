e53870ba98f8ac2b149f8e2683817c8969dc930f
Zeynep Tok <zeyneptok1997@gmail.com>
2022-08-16 20:39:04 +0300

    Initial Commit

---
+++ GitXcode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitXcode
+//
+//  Created by zeynep on 16.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitXcode/GitXcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitXcodeApp.swift
+//  GitXcode
+//
+//  Created by zeynep on 16.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct GitXcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
